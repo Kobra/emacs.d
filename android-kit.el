bef684f34ba538d9200c5fc2ddeaf07183112e1b
Kobra Lopatich (big-apple) <lopatich@onmail.ru>
2013-07-27 19:58:49 +0400

    add android-mode with android sdk

---
+++ android-kit.el
@@ -0,0 +1,7 @@
+(add-to-list 'load-path "~/github/android-mode/")
+(require 'android-mode)
+(custom-set-variables '(android-mode-sdk-dir "~/github/android-mode/"))
+(add-to-list 'load-path "~/Android/sdk/tools/lib")
+(require 'android)
+
+(provide 'android-kit)
